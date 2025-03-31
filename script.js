3896e435f8c738fecf6a41b9181fa622e556d060
CYBER-99 <126309291+CYBER-99@users.noreply.github.com>
2025-03-31 13:13:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ const turnOnLightsButton = document.querySelector('.turn-on-lights');
 const body = document.querySelector('body');
 const hiddenMessage = document.querySelector('.hidden-message');
 const candle = document.querySelector('.candle');
+const balloon = document.querySelector('.balloon');
 
 turnOnLightsButton.addEventListener('click', () => {
     body.style.backgroundColor = '#fff';
     hiddenMessage.style.display = 'block';
     candle.src = 'lit-candle.png';
+    balloon.style.display = 'block';
 });
+
